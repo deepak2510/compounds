7fb0c158b937153c8b051252da6d9981e766a5e6
Aaron Kaka <aaron.kaka@pearson.com>
2016-02-01 14:37:53 -0700

    Comment the readme.sh.

--- npm_scripts/readme.sh
+++ npm_scripts/readme.sh
@@ -1,7 +1,15 @@
 #!/bin/bash -e
+
+# Obtain the current working branch name in git
 branch="$(git rev-parse --abbrev-ref HEAD)"
 echo Update README TravisCI status to branch: "${branch}"
+
+# Search and replace the branch name in the README's TravisCI status URL
 sed -i '' 's/branch=[^/]*)/branch='${branch}')/g' README.md
+
+# If README has changed, commit the change - otherwise exit script without error
 git add README.md
-git commit -m "Update README TravisCI status to current branch."
+git diff --quiet --exit-code --cached || git commit -m "Update README TravisCI status to current branch."
+
+# Push the committed change to the branch
 git push origin ${branch}
